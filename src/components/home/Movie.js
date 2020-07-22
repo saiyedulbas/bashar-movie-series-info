a1358c87f92a67541407f755f42e831233fc7835
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 19:26:27 +0600

    Update Movie.js

--- src/components/home/Movie.js
+++ src/components/home/Movie.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+      import React, { Component } from 'react';
+              import { connect } from 'react-redux';
+              import { Link } from 'react-router-dom';
 
-import { fetchMovie, setLoading } from '../../actions/searchActions';
+              import { fetchMovie, setLoading } from '../../actions/searchActions';
 
-import Spinner from '../layout/Spinner';
+              import Spinner from '../layout/Spinner';
 
-export class Movie extends Component {
-  componentDidMount() {
-    this.props.fetchMovie(this.props.match.params.id);
-    this.props.setLoading();
-  }
-  render() {
-    const { loading, movie } = this.props;
+              export class Movie extends Component {
+        componentDidMount() {
+          this.props.fetchMovie(this.props.match.params.id);
+          this.props.setLoading();
+        }
+        render() {
+          const { loading, movie } = this.props;
 
     let movieInfo = (
       <div className="container">
@@ -20,28 +20,28 @@ export class Movie extends Component {
           <div className="col-md-4 card bg-danger card-body">
             <img src={movie.Poster} className="thumbnail" alt="Poster" />
           </div>
-          <div className="col-md-8">
-            <h2 className="mb-4">{movie.Title}</h2>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <strong>Genre:</strong> {movie.Genre}
-              </li>
-              <li className="list-group-item">
-                <strong>Released:</strong> {movie.Released}
-              </li>
-              <li className="list-group-item">
-                <strong>Rated:</strong> {movie.Rated}
-              </li>
-              <li className="list-group-item">
-                <strong>IMDB Rating:</strong> {movie.imdbRating}
-              </li>
-              <li className="list-group-item">
-                <strong>Director:</strong> {movie.Director}
-              </li>
-              <li className="list-group-item">
-                <strong>Writer:</strong> {movie.Writer}
-              </li>
-              <li className="list-group-item">
+                    <div className="col-md-8">
+                      <h2 className="mb-4">{movie.Title}</h2>
+                      <ul className="list-group">
+                        <li className="list-group-item">
+                          <strong>Genre:</strong> {movie.Genre}
+                        </li>
+                        <li className="list-group-item">
+                          <strong>Released:</strong> {movie.Released}
+                        </li>
+                        <li className="list-group-item">
+                      <strong>Rated:</strong> {movie.Rated}
+                    </li>
+                    <li className="list-group-item">
+                      <strong>IMDB Rating:</strong> {movie.imdbRating}
+                    </li>
+                    <li className="list-group-item">
+                      <strong>Director:</strong> {movie.Director}
+                    </li>
+                    <li className="list-group-item">
+                      <strong>Writer:</strong> {movie.Writer}
+                    </li>
+                    <li className="list-group-item">
                 <strong>Actors:</strong> {movie.Actors}
               </li>
             </ul>
@@ -50,37 +50,37 @@ export class Movie extends Component {
         <div className="row">
           <div className="card card-body bg-danger my-5 text-light">
             <div className="col-md-12">
-              <h3>About </h3>
-              {movie.Plot}
-              <hr />
-              <a
-                href={'https://www.imdb.com/title/' + movie.imdbID}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-primary"
-              >
-                View on IMDB
-              </a>
-              <Link to="/" className="btn btn-default text-light">
-                Go Back To Search
-              </Link>
+                          <h3>About </h3>
+                          {movie.Plot}
+                          <hr />
+                          <a
+                            href={'https://www.imdb.com/title/' + movie.imdbID}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn btn-primary"
+                          >
+                            View on IMDB
+                          </a>
+                          <Link to="/" className="btn btn-default text-light">
+                            Go Back To Search
+                          </Link>
             </div>
           </div>
-        </div>
-      </div>
-    );
+                    </div>
+                  </div>
+                );
 
-    let content = loading ? <Spinner /> : movieInfo;
-    return <div>{content}</div>;
-  }
-}
+                let content = loading ? <Spinner /> : movieInfo;
+                return <div>{content}</div>;
+              }
+            }
 
-const mapStateToProps = state => ({
-  loading: state.movies.loading,
-  movie: state.movies.movie
+            const mapStateToProps = state => ({
+              loading: state.movies.loading,
+              movie: state.movies.movie
 });
 
-export default connect(
-  mapStateToProps,
-  { fetchMovie, setLoading }
-)(Movie);
+              export default connect(
+                mapStateToProps,
+                { fetchMovie, setLoading }
+              )(Movie);
